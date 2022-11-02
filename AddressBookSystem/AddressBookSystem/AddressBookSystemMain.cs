67b63a1e4b60d2da1d501a7e77c1f2941f68994e
DibyaLochan-Pahi <dibyalochan.pahi@gmail.com>
2022-11-02 23:56:29 +0530

    Added UC1 Program Address Book System

---
+++ AddressBookSystem/AddressBookSystem/AddressBookSystemMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AddressBookSystem
+{
+    public class AddressBookSystemMain
+    {
+
+        //Default Constructor For Print Welcome Message
+        public AddressBookSystemMain()
+        {
+            Console.WriteLine("Welcome to Address Book Program !!!");
+        }
+
+        public static void Main(String[] args)
+        {
+
+            //Initialize Object
+            AddressBookSystemMain obj = new AddressBookSystemMain();
+
+            //Calling Encapsulated Class object
+            ContactPerson addressBook = new ContactPerson("DibyaLochan", "Pahi", "Jagdaa", "Rourkela",
+                    "Odisha", "769008", "9658427217", "dibyalochan.pahi@gmail.com");
+
+            Console.WriteLine("\n" + addressBook.toString());
+        }
+    }
+}
---
+++ AddressBookSystem/AddressBookSystem/ContactPerson.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    public class ContactPerson
+    {
+        //Declaring variables for Address Book System
+
+        private String firstName;
+        private String lastName;
+        private String address;
+        private String city;
+        private String state;
+        private String zip;
+        private String phoneNumber;
+        private String email;
+
+        //Declaring a parameterized constructor by means of which we can set up the values for instance variables above.
+        public ContactPerson(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber, String email)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+        }
+
+
+        //Creating toString methods to grab the values.
+        //Override
+        public String toString()
+        {
+            return "\n AddressBook" +
+                   "\n ------------" +
+                    "\n FirstName=" + firstName +
+                    "\n LastName=" + lastName +
+                    "\n Address=" + address +
+                    "\n City=" + city +
+                    "\n State=" + state +
+                    "\n Zip=" + zip +
+                    "\n PhoneNumber=" + phoneNumber +
+                    "\n Email=" + email;
+
+        }
+
+    }
+}
